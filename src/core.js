74735bbc19d15b385e256dd9f2acb5458a3d4653
Matt Harnetiaux <mattskyh@gmail.com>
2017-05-10 15:04:03 -0700

    updates to actions and tests

--- src/core.js
+++ src/core.js
@@ -1,6 +1,37 @@
-import {List} from 'immutable';
+import {List,Map} from 'immutable';
 
 export function set_entries(state,entries){
     "use strict";
     return state.set('entries',List(entries));
 }
+
+export function next(state){
+    "use strict";
+    const entries = state.get('entries');
+    return state.merge({
+        vote:Map(
+            {
+                pair: entries.take(2)
+            }
+        ),
+        entries:entries.skip(2)
+    });
+}
+
+export function vote(state,entry){
+    "use strict";
+    return state.updateIn(
+        ['vote','tally',entry],
+        0,
+        tally => tally + 1
+    );
+}
+
+
+
+
+
+
+
+
+
--- test/core_spec.js
+++ test/core_spec.js
@@ -1,18 +1,85 @@
 import {expect} from 'chai';
 import {List,Map} from 'immutable';
-import {set_entries} from '../src/core';
+import {set_entries,next,vote} from '../src/core';
 
 describe('bounty logic', () => {
     "use strict";
+
     describe('set_entries',() => {
         it('adds the entries to the state', () => {
-            const state = Map(),
-                entries = ['a','b'],
-                next_state = set_entries(state,entries);
+            const state = Map();
+            const entries = List.of('a','b');
+            const next_state = set_entries(state,entries);
+            const test_data = Map({
+                entries:List.of('a','b')
+            });
 
-            expect(next_state).to.equal(Map({
-                entries:List.of('a,b')
-            }));
+            expect(next_state).to.equal(test_data);
+        });
+    });
+
+    describe('next', () => {
+        const state = Map({
+            entries:List.of('a','b','c')
+        });
+        const test_data = Map({
+            vote:Map({
+                pair: List.of('a','b')
+            }),
+            entries:List.of('c')
+        });
+        const next_state = next(state);
+
+        expect(next_state).to.equal(test_data)
+    });
+
+    describe('vote', () => {
+
+        it('contains a tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('a', 'b')
+                }),
+                entries: List()
+            });
+            const next_state = vote(state, 'a');
+            const test_data = Map({
+                vote: Map({
+                    pair: List.of('a', 'b'),
+                    tally: Map({
+                        'a': 1
+                    })
+                }),
+                entries: List()
+            });
+
+            expect(next_state).to.equal(test_data);
+        });
+
+        it('adds to existing tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('a', 'b'),
+                    tally: Map({
+                        'a': 3,
+                        'b': 2
+                    })
+                }),
+                entries: List()
+            });
+            const next_state = vote(state, 'a');
+            const test_data = Map({
+                vote: Map({
+                    pair: List.of('a', 'b'),
+                    tally: Map({
+                        'a': 4,
+                        'b': 2
+                    })
+                }),
+                entries: List()
+            });
+
+            expect(next_state).to.equal(test_data);
         });
     });
 });
